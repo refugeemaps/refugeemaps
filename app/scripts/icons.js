46dcd11c7e5ec2a801a19c7b3398f9cfc46cdcce
Malte Modrow <modrow@ubilabs.net>
2015-09-10 14:31:07 +0200

    docs(icons): add missing jsdocs

--- app/scripts/icons.js
+++ app/scripts/icons.js
@@ -2,6 +2,11 @@
 
 export default class Icons {
 
+  /**
+   * Get the marker icon
+   * @param {String} type The type of the location
+   * @return {GoogleIconObject} icon The marker icon
+   */
   getIconByType(type) {
     switch (type) {
     case 'user':
@@ -15,6 +20,10 @@ export default class Icons {
     }
   }
 
+  /**
+   * Get the user icon
+   * @return {GoogleIconObject} icon The marker icon
+   */
   getUserIcon() {
     let icon = this.create({
       url: '../assets/marker-stroked-24@2x.png',
@@ -26,6 +35,10 @@ export default class Icons {
     return icon;
   }
 
+  /**
+   * Get the supermarket icon
+   * @return {GoogleIconObject} icon The marker icon
+   */
   getSupermarketIcon() {
     let icon = this.create({
       url: '../assets/grocery-24@2x.png',
@@ -37,6 +50,10 @@ export default class Icons {
     return icon;
   }
 
+  /**
+   * Get the culture icon
+   * @return {GoogleIconObject} icon The marker icon
+   */
   getCultureIcon() {
     let icon = this.create({
       url: '../assets/town-hall-24@2x.png',
@@ -48,6 +65,10 @@ export default class Icons {
     return icon;
   }
 
+  /**
+   * Get the default icon
+   * @return {GoogleIconObject} icon The marker icon
+   */
   getDefaultIcon() {
     let icon = this.create({
       url: '../assets/marker-24@2x.png',
@@ -59,6 +80,15 @@ export default class Icons {
     return icon;
   }
 
+  /**
+   * Create the google icon object
+   * @param {String} url The icon URL
+   * @param {Integer} width The icon width
+   * @param {Integer} height The icon height
+   * @param {Integer} anchorX The x anchor value
+   * @param {Integer} anchorY The y anchor value
+   * @return {GoogleIconObject} icon The marker icon
+   */
   create({url, width, height, anchorX, anchorY}) {
     let image = {
       url: url,
