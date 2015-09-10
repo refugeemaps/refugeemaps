ae4c22ad801bf1f745944e2ddd761a582e9ceaab
Malte Modrow <modrow@ubilabs.net>
2015-09-10 18:17:24 +0200

    refactor(icons): simplify and shorten code

--- app/scripts/icons.js
+++ app/scripts/icons.js
@@ -13,37 +13,37 @@ export default class Icons {
     case 'user':
       return this.getUserIcon();
     case 'mosque':
-      return this.getMosqueIcon();
+      return this.create({url: '../assets/religious-muslim-24@2x.png'});
     case 'church':
-      return this.getChurchIcon();
+      return this.create({url: '../assets/religious-christian-24@2x.png'});
     case 'bank':
-      return this.getBankIcon();
+      return this.create({url: '../assets/bank-24@2x.png'});
     case 'call-shop':
-      return this.getCallshopIcon();
+      return this.create({url: '../assets/telephone-24@2x.png'});
     case 'supermarket':
-      return this.getSupermarketIcon();
+      return this.create({url: '../assets/grocery-24@2x.png'});
     case 'culture':
-      return this.getCultureIcon();
+      return this.create({url: '../assets/town-hall-24@2x.png'});
     case 'pharmacy':
-      return this.getPharmacyIcon();
+      return this.create({url: '../assets/pharmacy-24@2x.png'});
     case 'laundry':
-      return this.getLaundryIcon();
+      return this.create({url: '../assets/laundry-24@2x.png'});
     case 'playground':
-      return this.getPlaygroundIcon();
+      return this.create({url: '../assets/playground-24@2x.png'});
     case 'football':
-      return this.getFootballIcon();
+      return this.create({url: '../assets/soccer-24@2x.png'});
     case 'basketball':
-      return this.getBasketballIcon();
+      return this.create({url: '../assets/basketball-24@2x.png'});
     case 'table-tennis':
-      return this.getTableTennisIcon();
+      return this.create({url: '../assets/tennis-24@2x.png'});
     case 'skateboard':
-      return this.getSkateboardIcon();
+      return this.create({url: '../assets/skateboard.png'});
     case 'library':
-      return this.getLibraryIcon();
+      return this.create({url: '../assets/library-24@2x.png'});
     case 'public-restroom':
-      return this.getRestroomIcon();
+      return this.create({url: '../assets/toilets-24@2x.png'});
     default:
-      return this.getDefaultIcon();
+      return this.create({url: '../assets/marker-24@2x.png'});
     }
   }
   /* eslint-enable complexity */
@@ -64,246 +64,6 @@ export default class Icons {
   }
 
   /**
-   * Get the mosque icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getMosqueIcon() {
-    let icon = this.create({
-      url: '../assets/religious-muslim-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the church icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getChurchIcon() {
-    let icon = this.create({
-      url: '../assets/religious-christian-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the bank icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getBankIcon() {
-    let icon = this.create({
-      url: '../assets/bank-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the call-shop icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getCallshopIcon() {
-    let icon = this.create({
-      url: '../assets/telephone-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the supermarket icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getSupermarketIcon() {
-    let icon = this.create({
-      url: '../assets/grocery-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the culture icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getCultureIcon() {
-    let icon = this.create({
-      url: '../assets/town-hall-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the pharmacy icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getPharmacyIcon() {
-    let icon = this.create({
-      url: '../assets/pharmacy-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the laundry icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getLaundryIcon() {
-    let icon = this.create({
-      url: '../assets/laundry-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the playground icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getPlaygroundIcon() {
-    let icon = this.create({
-      url: '../assets/playground-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the football icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getFootballIcon() {
-    let icon = this.create({
-      url: '../assets/soccer-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the basketball icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getBasketballIcon() {
-    let icon = this.create({
-      url: '../assets/basketball-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the table-tennis icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getTableTennisIcon() {
-    let icon = this.create({
-      url: '../assets/tennis-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the skateboard icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getSkateboardIcon() {
-    let icon = this.create({
-      url: '../assets/skateboard.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the library icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getLibraryIcon() {
-    let icon = this.create({
-      url: '../assets/library-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the restroom icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getRestroomIcon() {
-    let icon = this.create({
-      url: '../assets/toilets-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
-   * Get the default icon
-   * @return {GoogleIconObject} icon The marker icon
-   */
-  getDefaultIcon() {
-    let icon = this.create({
-      url: '../assets/marker-24@2x.png',
-      width: 30,
-      height: 30,
-      anchorX: 15,
-      anchorY: 15
-    });
-    return icon;
-  }
-
-  /**
    * Create the google icon object
    * @param {String} url The icon URL
    * @param {Integer} width The icon width
@@ -312,7 +72,7 @@ export default class Icons {
    * @param {Integer} anchorY The y anchor value
    * @return {GoogleIconObject} icon The marker icon
    */
-  create({url, width, height, anchorX, anchorY}) {
+  create({url, width = 30, height = 30, anchorX = 15, anchorY = 15}) {
     let image = {
       url: url,
       size: new google.maps.Size(width, height),
