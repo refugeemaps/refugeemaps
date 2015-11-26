4a1546f58935eff16e347f3f034ba4e41a0539e9
Robert Katzki <katzki@ubilabs.net>
2015-11-26 17:19:27 +0100

    refactor(locations): simplify getUsedLanguages and getUsedCategories

--- src/lib/locations/location-categories.go
+++ src/lib/locations/location-categories.go
@@ -10,27 +10,17 @@ func (location Location) GetUsedCategories(c appengine.Context) (usedCategories 
 	allPois := location.GetPois(c)
 	allCategories := categories.All(c)
 
-	var categoryMap = make(map[string]bool)
-
 	for _, category := range allCategories {
 		if category.Key == "all" {
-			categoryMap[category.Key] = true
-		} else {
-			categoryMap[category.Key] = false
-		}
-	}
-
-	for _, poi := range allPois {
-		for _, category := range allCategories {
-			if poi.Category == category.Key {
-				categoryMap[category.Key] = true
-			}
-		}
-	}
-
-	for _, category := range allCategories {
-		if categoryMap[category.Key] {
 			usedCategories = append(usedCategories, category)
+			continue
+		}
+
+		for _, poi := range allPois {
+			if poi.Category == category.Key {
+				usedCategories = append(usedCategories, category)
+				break
+			}
 		}
 	}
 
